9cc7f123531f85d3a4fe75337047a1f2f2f015f6
Michael Coxeter <mcoxeter@gamil.com>
2020-09-21 20:47:28 +0200

    Added storm images

--- src/bug.ts
+++ src/bug.ts
@@ -1,94 +1,92 @@
-import "phaser";
+import 'phaser';
 import { BugFalling } from './bugFalling';
 import { IModelBugInfo, SceneMain } from './sceneMain';
 
 export class Bug extends Phaser.Physics.Arcade.Sprite {
-    scale = 1;
-    vector: Phaser.Math.Vector2 = undefined;
-    speed = 1;
-    bugBoardBounds: Phaser.Geom.Rectangle = undefined;
-    elapsedTime = 0;
-    timeToDie = 0;
-    constructor(
-        public sceneMain: SceneMain, 
-        public color: string,
-        bounds: Phaser.Geom.Rectangle,
-        public bugInfo: IModelBugInfo
-        ) {
-      super(sceneMain, 0, 0, `bug_${color}`);
-      sceneMain.add.existing(this);
-      sceneMain.physics.add.existing(this);
-      sceneMain.healthyBugs = sceneMain.healthyBugs.concat(this);
-      this.bugBoardBounds = bounds;
-      const pos = bounds.getRandomPoint();
-      this.setPosition(pos.x, pos.y);
-      const animationName = `bug_${color}_animation`;
-      this.play(animationName);
-      this.scale = .15 + (5 - bugInfo.severity) *.15;
-      this.timeToDie = Math.random() * 10000;
-      this.vector = new Phaser.Math.Vector2(
-        Math.random() * 40 - 20,
-        Math.random() * 40 - 20
-      );
-      this.speed = Math.random() + 1;
-      this.setGravityY(0);
-  
-      this.setScale(this.scale);
-    }
-    create() {}
-    update(time: number, delta: number) {
-      this.elapsedTime += delta;
+  scale = 1;
+  vector: Phaser.Math.Vector2 = undefined;
+  speed = 1;
+  bugBoardBounds: Phaser.Geom.Rectangle = undefined;
+  elapsedTime = 0;
+  timeToDie = 0;
+  constructor(
+    public sceneMain: SceneMain,
+    public color: string,
+    bounds: Phaser.Geom.Rectangle,
+    public bugInfo: IModelBugInfo
+  ) {
+    super(sceneMain, 0, 0, `bug_${color}`);
+    sceneMain.add.existing(this);
+    sceneMain.physics.add.existing(this);
+    sceneMain.healthyBugs = sceneMain.healthyBugs.concat(this);
+    this.bugBoardBounds = bounds;
+    const pos = bounds.getRandomPoint();
+    this.setPosition(pos.x, pos.y);
+    const animationName = `bug_${this.color}_animation`;
+    this.play(animationName);
+    this.scale = 0.15 + (5 - bugInfo.severity) * 0.15;
+    this.timeToDie = Math.random() * 10000;
+    this.vector = new Phaser.Math.Vector2(
+      Math.random() * 40 - 20,
+      Math.random() * 40 - 20
+    );
+    this.speed = Math.random() + 1;
 
-      if( this.bugInfo.fixed && this.elapsedTime > this.timeToDie ){
-        new BugFalling(this.sceneMain, this);
-        const myIndex = this.sceneMain.healthyBugs.indexOf(this);
-        this.sceneMain.healthyBugs.splice(myIndex,1);
-        this.destroy();
-        return;
-      }
-
-      var v = new Phaser.Math.Vector2(this.vector.x, this.vector.y);
-      if (this.x > this.bugBoardBounds.width + this.bugBoardBounds.x) {
-        this.x = this.bugBoardBounds.width + this.bugBoardBounds.x;
-        this.vector = v.rotate(0.5);
-      } else if (this.y > this.bugBoardBounds.height + this.bugBoardBounds.y) {
-        this.y = this.bugBoardBounds.height + this.bugBoardBounds.y;
-        this.vector = v.rotate(0.5);
-      } else if (this.x < this.bugBoardBounds.x) {
-        this.x = this.bugBoardBounds.x;
-        this.vector = v.rotate(0.5);
-      } else if (this.y < this.bugBoardBounds.y) {
-        this.y = this.bugBoardBounds.y;
-        this.vector = v.rotate(0.5);
-      }
-  
-      this.x += (this.vector.x / 1000) * (delta * this.speed);
-      this.y += (this.vector.y / 1000) * (delta * this.speed);
-  
-      this.setAngle(this.vector.angle() * Phaser.Math.RAD_TO_DEG + 90.0);
-    }
-  
-    beginDeathMarch() {
-      new BugFalling(this.sceneMain, this);
-    }
-  
-    static createAnimation(sceneMain: SceneMain, color: string): void {
-        sceneMain.anims.create({
-        key: `bug_${color}_animation`,
-        frames: sceneMain.anims.generateFrameNumbers(`bug_${color}`, {}),
-        frameRate: 5, // frames per second
-        repeat: -1, // -1 for infinite loops
-      });
-    }
-    static loadSprite(sceneMain: SceneMain, color: string): void {
-        sceneMain.load.spritesheet(
-        `bug_${color}`,
-        `assets/spritesheets/bugs-${color}-128x128.png`,
-        {
-          frameWidth: 128,
-          frameHeight: 128,
-        }
-      );
-    }
+    this.setScale(this.scale);
   }
-  
+  create() {}
+  update(time: number, delta: number) {
+    this.elapsedTime += delta;
+
+    if (this.bugInfo.fixed && this.elapsedTime > this.timeToDie) {
+      new BugFalling(this.sceneMain, this);
+      const myIndex = this.sceneMain.healthyBugs.indexOf(this);
+      this.sceneMain.healthyBugs.splice(myIndex, 1);
+      this.destroy();
+      return;
+    }
+
+    var v = new Phaser.Math.Vector2(this.vector.x, this.vector.y);
+    if (this.x > this.bugBoardBounds.width + this.bugBoardBounds.x) {
+      this.x = this.bugBoardBounds.width + this.bugBoardBounds.x;
+      this.vector = v.rotate(0.5);
+    } else if (this.y > this.bugBoardBounds.height + this.bugBoardBounds.y) {
+      this.y = this.bugBoardBounds.height + this.bugBoardBounds.y;
+      this.vector = v.rotate(0.5);
+    } else if (this.x < this.bugBoardBounds.x) {
+      this.x = this.bugBoardBounds.x;
+      this.vector = v.rotate(0.5);
+    } else if (this.y < this.bugBoardBounds.y) {
+      this.y = this.bugBoardBounds.y;
+      this.vector = v.rotate(0.5);
+    }
+
+    this.x += (this.vector.x / 1000) * (delta * this.speed);
+    this.y += (this.vector.y / 1000) * (delta * this.speed);
+
+    this.setAngle(this.vector.angle() * Phaser.Math.RAD_TO_DEG + 90.0);
+  }
+
+  beginDeathMarch() {
+    new BugFalling(this.sceneMain, this);
+  }
+
+  static createAnimation(sceneMain: SceneMain, color: string): void {
+    sceneMain.anims.create({
+      key: `bug_${color}_animation`,
+      frames: sceneMain.anims.generateFrameNumbers(`bug_${color}`, {}),
+      frameRate: 5, // frames per second
+      repeat: -1, // -1 for infinite loops
+    });
+  }
+  static loadSprite(sceneMain: SceneMain, color: string): void {
+    sceneMain.load.spritesheet(
+      `bug_${color}`,
+      `assets/spritesheets/bugs-${color}-128x128.png`,
+      {
+        frameWidth: 128,
+        frameHeight: 128,
+      }
+    );
+  }
+}
--- src/sceneMain.ts
+++ src/sceneMain.ts
@@ -2,174 +2,187 @@ import { Bug } from './bug';
 import { BugFalling } from './bugFalling';
 
 export class SceneMain extends Phaser.Scene {
-    colors = ['blue', 'green', 'magenta', 'purple', 'red'];
-    healthyBugs: Bug[] = [];
-    bugsFalling: BugFalling[] = [];
-    bugBoardBoundary = new Phaser.Geom.Rectangle(340, 100, 600, 300);
-    team = 'All Bugs';
-    model: IModel = { bugs: []}
-  
-    constructor() {
-      super('game');
-    }
-  
-    preload() {
-      this.load.spritesheet(
-        'baseball',
-        'assets/spritesheets/baseball-spritesheet-640x470.png',
-        {
-          frameWidth: 640,
-          frameHeight: 470,
-        }
-      );
+  colors = ['blue', 'green', 'magenta', 'purple', 'red'];
+  healthyBugs: Bug[] = [];
+  bugsFalling: BugFalling[] = [];
+  bugBoardBoundary = new Phaser.Geom.Rectangle(340, 100, 600, 300);
+  team = 'All Bugs';
+  model: IModel = { bugs: [] };
 
-      const paramPair = document.URL.split('?')[1];
-      if( paramPair ){
-        
-          this.team = unescape(paramPair.split('=')[1]);
-      }
-  
-      for (var color of this.colors) {
-        Bug.loadSprite(this, color);
-        BugFalling.loadSprite(this,color);
-      }
-  
-      this.load.image('bugboard', 'assets/images/bugboard.png');
-      this.load.image('backdrop', 'assets/images/backdrop.png');
-      this.load.image('cloud1', 'assets/images/cloud1.png');
-      this.load.image('cloud2', 'assets/images/cloud2.png');
-      this.load.image('development', 'assets/images/development.png');
-      this.load.image('qa', 'assets/images/qa.png');
-      this.load.image('fixed', 'assets/images/fixed.png');
-    }
-  
-    async create(): Promise<void> {
-      var backdrop = this.add.image(0, 0, 'backdrop');
-      backdrop.setOrigin(0, 0);
-      const bugBoard = this.add.image(650, 300, 'bugboard');
-      bugBoard.setScale(1.2);
-      const apiResults = await this.getNumberOfBugs();
-
-      const filteredResults: IAPIResult = {
-          bugs:apiResults.bugs.filter( b => this.team === 'All Bugs' || this.team === b.team)
-      };
-
-      const text = this.add.text(10,10,
-        this.team +
-        "\nPopulation: " + filteredResults.bugs.length +
-        "\nClosed Today: " + filteredResults.bugs.filter( b => b.status === 'Closed' || b.status === 'Released').length        
-        );
-      text.setColor("Black");
-      text.setFont("Emmett");
-      text.setFontSize(24);
-
-      this.model = this.mapResultToModel(filteredResults);
-      for (var bugIndex = 0; bugIndex < filteredResults.bugs.length; bugIndex++) {
-        new Bug(this, this.colors[bugIndex % 5], this.bugBoardBoundary, this.model.bugs[bugIndex]);
-      }
-  
-      for (var color of this.colors) {
-        Bug.createAnimation(this, color);
-        BugFalling.createAnimation(this,color)
-      }
-
-    }
-
-    mapResultToModel(result: IAPIResult): IModel {
-      const bugs = result.bugs.map(x => {
-        const assignee = x.assignee;
-        let severity = 0;
-        switch( x.priority){
-          case 'Blocker':
-            severity = 1;
-            break;
-          case 'Critical':
-            severity = 2;
-            break;
-          case 'Major':
-            severity = 3;
-            break;
-          case 'Minor':
-            severity = 4;
-            break;
-          default:
-            severity = 4;
-        }
-        const team = x.team;
-        return {
-          assignee: x.assignee,
-          id: x.key,
-          fixed: x.status === 'Closed' || x.status === 'Released',
-          severity,
-          date: x.updated, 
-          team: x.team       
-        }
-      } );
-      return {
-        bugs: bugs
-      }
-
-    }
-    
-    async update(time: number, delta: number): Promise<void> {
-      for (var bug of this.healthyBugs) {
-        bug.update(time, delta);
-      }
-      for (var bugOnMarch of this.bugsFalling) {
-        bugOnMarch.update(time, delta);
-      }
-    }
-
-    async testGetNumberOfBugs(): Promise<IAPIResult> {
-        return new Promise((resolve) =>
-          setTimeout(() => {
-            resolve({
-              bugs: [
-                  {
-                    key: "PURESUPPORT-68354",
-                    summary: "Implement automated search using ResearcherID with WoSlite.",
-                    assignee: "Team Data Models",
-                    assigneeIconUrl: "https://support.pure.elsevier.com/secure/useravatar?ownerId=team_data_models&avatarId=15593",
-                    status: "Open",
-                    team: "Data models",
-                    priority: "Critical",
-                    updated: "2020-09-21T10:30:32.000+00:00"                    
-                  },
-                  {
-                    key: "PURESUPPORT-68353",
-                    summary: "REF2021 - REF2 Output Label",
-                    assignee: "Claus Poulsen",
-                    assigneeIconUrl: "https://support.pure.elsevier.com/secure/useravatar?ownerId=cpo&avatarId=16387",
-                    status: "Open",
-                    team: "Support",
-                    priority: "Major",
-                    updated: "2020-09-21T10:33:53.000+00:00"
-                  }
-            ]
-          })
-          }, 1000)
-        );
-      }    
-      async getNumberOfBugs(): Promise<IAPIResult> {
-        const liveUrl = 'http://localhost:9090/api/bugboard';
-        const testUrl = '/assets/testApiResponse.json';
-         const result = await fetch(liveUrl).then( response => response.json() );
-         return {
-           bugs: result 
-         }
-       }    
+  constructor() {
+    super('game');
   }
 
+  preload() {
+    this.load.spritesheet(
+      'baseball',
+      'assets/spritesheets/baseball-spritesheet-640x470.png',
+      {
+        frameWidth: 640,
+        frameHeight: 470,
+      }
+    );
+
+    const paramPair = document.URL.split('?')[1];
+    if (paramPair) {
+      this.team = unescape(paramPair.split('=')[1]);
+    }
+
+    for (var color of this.colors) {
+      Bug.loadSprite(this, color);
+      BugFalling.loadSprite(this, color);
+    }
+
+    this.load.image('bugboard', 'assets/images/bugboard.png');
+    this.load.image('backdrop', 'assets/images/backdrop.png');
+    this.load.image('cloud1', 'assets/images/cloud1.png');
+    this.load.image('cloud2', 'assets/images/cloud2.png');
+    this.load.image('development', 'assets/images/development.png');
+    this.load.image('qa', 'assets/images/qa.png');
+    this.load.image('fixed', 'assets/images/fixed.png');
+  }
+
+  async create(): Promise<void> {
+    var backdrop = this.add.image(0, 0, 'backdrop');
+    backdrop.setOrigin(0, 0);
+    const bugBoard = this.add.image(650, 300, 'bugboard');
+    bugBoard.setScale(1.2);
+    const apiResults = await this.getNumberOfBugs();
+
+    const filteredResults: IAPIResult = {
+      bugs: apiResults.bugs.filter(
+        (b) => this.team === 'All Bugs' || this.team === b.team
+      ),
+    };
+
+    const text = this.add.text(
+      10,
+      10,
+      this.team +
+        '\nPopulation: ' +
+        filteredResults.bugs.length +
+        '\nClosed Today: ' +
+        filteredResults.bugs.filter(
+          (b) => b.status === 'Closed' || b.status === 'Released'
+        ).length
+    );
+    text.setColor('Black');
+    text.setFont('Emmett');
+    text.setFontSize(24);
+
+    this.model = this.mapResultToModel(filteredResults);
+    for (var color of this.colors) {
+      Bug.createAnimation(this, color);
+      BugFalling.createAnimation(this, color);
+    }
+
+    for (var bugIndex = 0; bugIndex < filteredResults.bugs.length; bugIndex++) {
+      new Bug(
+        this,
+        this.colors[bugIndex % 5],
+        this.bugBoardBoundary,
+        this.model.bugs[bugIndex]
+      );
+    }
+  }
+
+  mapResultToModel(result: IAPIResult): IModel {
+    const bugs = result.bugs.map((x) => {
+      const assignee = x.assignee;
+      let severity = 0;
+      switch (x.priority) {
+        case 'Blocker':
+          severity = 1;
+          break;
+        case 'Critical':
+          severity = 2;
+          break;
+        case 'Major':
+          severity = 3;
+          break;
+        case 'Minor':
+          severity = 4;
+          break;
+        default:
+          severity = 4;
+      }
+      const team = x.team;
+      return {
+        assignee: x.assignee,
+        id: x.key,
+        fixed: x.status === 'Closed' || x.status === 'Released',
+        severity,
+        date: x.updated,
+        team: x.team,
+      };
+    });
+    return {
+      bugs: bugs,
+    };
+  }
+
+  async update(time: number, delta: number): Promise<void> {
+    for (var bug of this.healthyBugs) {
+      bug.update(time, delta);
+    }
+    for (var bugOnMarch of this.bugsFalling) {
+      bugOnMarch.update(time, delta);
+    }
+  }
+
+  async testGetNumberOfBugs(): Promise<IAPIResult> {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        resolve({
+          bugs: [
+            {
+              key: 'PURESUPPORT-68354',
+              summary:
+                'Implement automated search using ResearcherID with WoSlite.',
+              assignee: 'Team Data Models',
+              assigneeIconUrl:
+                'https://support.pure.elsevier.com/secure/useravatar?ownerId=team_data_models&avatarId=15593',
+              status: 'Open',
+              team: 'Data models',
+              priority: 'Critical',
+              updated: '2020-09-21T10:30:32.000+00:00',
+            },
+            {
+              key: 'PURESUPPORT-68353',
+              summary: 'REF2021 - REF2 Output Label',
+              assignee: 'Claus Poulsen',
+              assigneeIconUrl:
+                'https://support.pure.elsevier.com/secure/useravatar?ownerId=cpo&avatarId=16387',
+              status: 'Open',
+              team: 'Support',
+              priority: 'Major',
+              updated: '2020-09-21T10:33:53.000+00:00',
+            },
+          ],
+        });
+      }, 1000)
+    );
+  }
+  async getNumberOfBugs(): Promise<IAPIResult> {
+    const liveUrl = 'http://localhost:9090/api/bugboard';
+    const testUrl = '/assets/testApiResponse.json';
+    const result = await fetch(liveUrl).then((response) => response.json());
+    return {
+      bugs: result,
+    };
+  }
+}
+
 export interface IModel {
-    bugs: IModelBugInfo[];
+  bugs: IModelBugInfo[];
 }
 export interface IModelBugInfo {
-    assignee: string;
-    id: string;
-    fixed: boolean;
-    severity: number;
-    date: string;      
-    team: string;
+  assignee: string;
+  id: string;
+  fixed: boolean;
+  severity: number;
+  date: string;
+  team: string;
 }
 
 export interface IAPIResult {
